1f2401f4f294796b8568d9a09244a84041b3ba73
hameee <113837605+qwerty00ui88@users.noreply.github.com>
2023-11-09 11:50:43 +0900

    Lesson08

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -86,5 +87,15 @@ public class MemberController {
             memberRepository.save(memberEntity);
         }
         return "redirect:/members/" + memberEntity.getId();
+    }
+
+    @GetMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        Member target = memberRepository.findById(id).orElse(null);
+        if(target != null) {
+            memberRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제됐습니다!");
+        }
+        return "redirect:/members";
     }
 }
