2b5f026d2afc028d3d5eb6e7cff5f2db870fc39a
hameee <113837605+qwerty00ui88@users.noreply.github.com>
2023-11-12 14:40:13 +0900

    Lesson09

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO article(id, title, content) VALUES(1, '가가가가', '1111');
-INSERT INTO article(id, title, content) VALUES(2, '나나나나', '2222');
-INSERT INTO article(id, title, content) VALUES(3, '다다다다', '3333');
+INSERT INTO article(title, content) VALUES('가가가가', '1111');
+INSERT INTO article(title, content) VALUES('나나나나', '2222');
+INSERT INTO article(title, content) VALUES('다다다다', '3333');
 
-INSERT INTO member(id, email, password) VALUES(1, '1111@gmail.com', '1111');
-INSERT INTO member(id, email, password) VALUES(2, '2222@naver.com', '2222');
-INSERT INTO member(id, email, password) VALUES(3, '3333@gmail.com', '3333');
+INSERT INTO member(email, password) VALUES('1111@gmail.com', '1111');
+INSERT INTO member(email, password) VALUES('2222@naver.com', '2222');
+INSERT INTO member(email, password) VALUES('3333@gmail.com', '3333');
